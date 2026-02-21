196e81aab08640ea4f63aec086e5b43fccbccd42
songdongsun <9856824+song-dongsun@user.noreply.gitee.com>
2026-02-22 04:55:23 +0800

    fix:BIFPN结构：调整学习率—+patience

--- train_PT.py
+++ train_PT.py
@@ -23,10 +23,10 @@ if __name__ == "__main__":
 
     results = model.train(data="./tiantianquan/cfg/dataset_pt.yaml",
                           device=device,
-                          epochs = 200,
-                          patience = 15,
+                          epochs = 300,
+                          patience = 20,
                           optimizer='SGD',  # 指定优化器为AdamW
-                          lr0=0.0008,  # AdamW学习率（SGD的1/10）
+                          lr0=0.0025,  # AdamW学习率（SGD的1/10）
                           weight_decay=0.0005,
                           cos_lr=True,
                           )

6210ec9dea88f505f6b324e7dfd6dc6ac91600ee
songdongsun <9856824+song-dongsun@user.noreply.gitee.com>
2026-02-24 12:44:10 +0800

    fix:学习率：0.0001

--- train_PT.py
+++ train_PT.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
                           epochs = 300,
                           patience = 20,
                           optimizer='AdamW',  # 指定优化器为AdamW
-                          lr0=0.0001,  # AdamW学习率（SGD的1/10）
+                          lr0=0.0025,  # AdamW学习率（SGD的1/10）
                           weight_decay=0.0005,
                           cos_lr=True,
                           )
